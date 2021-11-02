4eeac11cbe78674f92dfafb018fa6e799e3298e9
Patrick Dolan <dolanp1992@gmail.com>
2021-11-01 19:40:32 -0700

    add divs with id's to each math operation to display results then added jquery to write them
    Co-authored-by: Ryan Rendon <Rendon.S.Ryan@gmail.com>
    Co-authored-by: Anastasia Han <anastasia.han@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,27 @@ $(document).ready(function() {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    alert(result);
+    $("#addResult").text(result);
+  });
+  $("#subtract").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#subtract1").val());
+    const number2 = parseInt($("#subtract2").val());
+    const result = subtract(number1, number2);
+    $("#subtractResult").text(result);
+  });
+  $("#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#multiplyResult").text(result);
+  });
+  $("#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#divideResult").text(result);
   });
 });
